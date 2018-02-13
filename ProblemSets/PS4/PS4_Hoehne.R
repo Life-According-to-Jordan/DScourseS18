76c133ef9e597daf2d834ae6bf6ad972df50b080
Econ Account <ouecon007@schooner1.oscer.ou.edu>
2018-02-13 12:48:50 -0600

    uploading PS3 and PS4

---
+++ ProblemSets/PS4/PS4_Hoehne.R
@@ -0,0 +1,6 @@
+library("jsonlite")
+system('wget -O nflstats.json "http://api.fantasy.nfl.com/v1/players/stats?statType=seasonStats&season=2010&week=1&format=json"')
+system('cat nflstats.json')
+mydf<-fromJSON("nflstats.json")
+class(mydf$players)
+head(mydf$players)
---
+++ ProblemSets/PS4/PS4b_Hoehne.R
@@ -0,0 +1,8 @@
+df1<-(iris)
+df<- createDataFrame(df1)
+class(df1)
+class(df)
+head(select(df, df$Sepal_Length, df$Species))
+head(filter(df, df$Sepal_Length>5.5))
+head(summarize(select(df, df$Sepal_Length, df$Species), filter(df, df$Sepal_Length>5.5)))
+head(summarize(groupBy(df, df$Species), mean=mean(df$Sepal_Length), count=n(df$Sepal_Length)))
